9429c9be94cf8e1909d2cc89a09e2df9bf8fc6ac
lunaceee <lunaceee@gmail.com>
2020-04-20 23:38:34 -0700

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from "vue";
+import Vuex from "vuex";
 import App from "./App.vue";
 
+Vue.use(Vuex);
 Vue.config.productionTip = false;
+
+export const store = new Vuex.Store({
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+});
 
 new Vue({
   render: (h) => h(App),
