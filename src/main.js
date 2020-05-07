af8969a6df1cf0c6900bd3c6d2881c9e263cf01f
lunaceee <lunaceee@gmail.com>
2020-05-07 14:41:29 -0700

    add vimeo support package

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vuex from "vuex";
 import App from "./App.vue";
 import router from "./router";
 import "video.js/dist/video-js.css";
+import '@devmobiliza/videojs-vimeo/dist/videojs-vimeo.esm';
 import { store } from "./store";
 
 export const bus = new Vue();
