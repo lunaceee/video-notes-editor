cd3612ce7323945bba5db686bb4834ae6211f7e9
lunaceee <lunaceee@gmail.com>
2020-05-14 15:50:46 -0700

    reverse notes order

--- src/store/index.js
+++ src/store/index.js
@@ -1,59 +1,59 @@
-import Vue from "vue";
-import Vuex from "vuex";
-import youtube from "videojs-youtube";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import youtube from 'videojs-youtube';
 
-"" + youtube; // hack for linter fixing
+'' + youtube; // hack for linter fixing
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-  state: { videos: [] },
-  getters: {
-    video: (state) => (url) => {
-      for (const video of state.videos) {
-        if (video.url === url) return video;
-      }
-    },
-  },
-  mutations: {
-    initializeStore: function () {
-      let persisted = localStorage.getItem("store");
-      if (persisted) {
-        persisted = JSON.parse(persisted) || {};
-      } else {
-        persisted = {};
-      }
+	state: { videos: [] },
+	getters: {
+		video: (state) => (url) => {
+			for (const video of state.videos) {
+				if (video.url === url) return video;
+			}
+		}
+	},
+	mutations: {
+		initializeStore: function() {
+			let persisted = localStorage.getItem('store');
+			if (persisted) {
+				persisted = JSON.parse(persisted) || {};
+			} else {
+				persisted = {};
+			}
 
-      if (!Array.isArray(persisted.videos)) {
-        persisted.videos = [];
-      }
+			if (!Array.isArray(persisted.videos)) {
+				persisted.videos = [];
+			}
 
-      this.replaceState(persisted);
-    },
+			this.replaceState(persisted);
+		},
 
-    addVideo: (state, newUrl) => {
-      const video = { url: newUrl, notes: [] };
-      state.videos = state.videos || [];
-      state.videos.push(video);
-    },
+		addVideo: (state, newUrl) => {
+			const video = { url: newUrl, notes: [] };
+			state.videos = state.videos || [];
+			state.videos.push(video);
+		},
 
-    addNote: (state, { video, time }) => {
-      video.notes.push({
-        text: "",
-        time
-      });
-    },
+		addNote: (state, { video, time }) => {
+			video.notes.unshift({
+				text: '',
+				time
+			});
+		},
 
-    updateNote: (state, { note, text }) => {
-      note.text = text;
-    },
+		updateNote: (state, { note, text }) => {
+			note.text = text;
+		},
 
-    deleteNote: (state, { video, noteIndex }) => {
-      video.notes.splice(noteIndex, 1)
-    }
-  },
+		deleteNote: (state, { video, noteIndex }) => {
+			video.notes.splice(noteIndex, 1);
+		}
+	}
 });
 
 // Subscribe to store updates, each update serialize to localStorage.
 store.subscribe((mutation, state) => {
-  localStorage.setItem("store", JSON.stringify(state));
+	localStorage.setItem('store', JSON.stringify(state));
 });
