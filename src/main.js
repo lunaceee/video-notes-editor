7b9ecd47fc387f1ab90d0e9be96790d343e4958e
lunaceee <lunaceee@gmail.com>
2020-06-08 15:40:13 -0700

    add normalize.css

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import 'normalize.css';
 
-import App from "./App.vue";
-import router from "./router";
-import "video.js/dist/video-js.css";
-import { store } from "./store";
+import App from './App.vue';
+import router from './router';
+import 'video.js/dist/video-js.css';
+import { store } from './store';
 
 export const bus = new Vue();
 
@@ -12,10 +13,10 @@ Vue.config.productionTip = false;
 Vue.use(Vuex);
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-  beforeCreate() {
-    store.commit("initializeStore");
-  },
-}).$mount("#app");
+	router,
+	store,
+	render: (h) => h(App),
+	beforeCreate() {
+		store.commit('initializeStore');
+	}
+}).$mount('#app');
