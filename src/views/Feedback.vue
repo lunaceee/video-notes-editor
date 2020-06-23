d12c589e711b137795e267becccd403698493938
lunaceee <lunaceee@gmail.com>
2020-06-22 17:21:40 -0700

    modify content

--- src/views/Feedback.vue
+++ src/views/Feedback.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="form-feedback">
-    <h2>Thanks for joining Cuttle!</h2>
-    <router-link :to="{ name: 'home' }">
-      Go to homepage
-    </router-link>
+    <h2>Thanks for joining Cuttle! Now you're able to create and share notes from your favorite videos!</h2>
+    <router-link :to="{ name: 'home' }">Go to homepage</router-link>
   </div>
 </template>
 <script>
 export default {
-  name: "feedback",
+  name: "feedback"
 };
 </script>
