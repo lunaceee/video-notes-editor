29e04833243281f67a3780bae3a6dee1a285bb70
lunaceee <lunaceee@gmail.com>
2020-10-06 22:25:58 -0700

    test edit icon without vue-svg-loader

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -10,7 +10,18 @@
       </button>
       <div>
         <button v-if="mode === 'showing'" @click="edit" class="px-2">
-          <IconEdit class="hover:text-red-400" />
+          <svg
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="stroke-current stroke-2 text-red-500 inline-block h-6 w-6"
+          >
+            <path
+              d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"
+            />
+            <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
+          </svg>
         </button>
         <button @click="deleteNote">
           <IconTrash class="hover:text-red-400" />
